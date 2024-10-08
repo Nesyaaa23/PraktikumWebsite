dfe1e008bafc24803421cc46d19883b6f88e99f7
Nesyaaa23 <nesyapramudia@gmail.com>
2024-10-08 23:45:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const button = document.getElementById('myButton');
-
+<tr>
+                <td>Salad Buah</td>
+                <td>Rp 15. 000</td>
+                <td><a href="https://www.briliofood.net/resep/10-cara-membuat-saus-salad-buah-creamy-enak-dan-praktis-191101o.html">Salad Buah</a></td>
+                <td><img src="https://tse3.mm.bing.net/th?id=OIP.CIGZ1a1hXrd7cw0uYntSCAHaHa&pid=Api&P=0&h=220.png" align="center" width="200px"height="150px"><br><br<td>
+            </tr>
+            <tr>
+                <td>Es Buah</td>
+                <td>Rp 15. 000</td>
+                <td><a href="https://www.astronauts.id/blog/daftar-resep-es-buah-yang-mudah-dan-enak/">Es Buah</a></td>
+                <td><img src="https://tse2.mm.bing.net/th?id=OIP.Gjs5Y4jj7Z-julEiSsxadQHaE8&pid=Api&P=0&h=220.png" align="center" width="200px"height="150px"><br><br<td>
+            </tr>
 button.addEventListener('click', () => {
   console.log('Tombol ditekan');
 });
