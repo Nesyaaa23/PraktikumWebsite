8e6c1fef30a55377ff9408a5d6e94f251b5c6a89
Nesyaaa23 <nesyapramudia@gmail.com>
2024-10-08 23:54:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
- document.write("Belajar Pemograman JavaScript!");
-       document.writeln("<center>");
-       document.writeln("Pancake");
-       document.writeln("Mile Crepes");
-       document.writeln("</center>");
 button.addEventListener('click', () => {
   console.log('Tombol ditekan');
 });
